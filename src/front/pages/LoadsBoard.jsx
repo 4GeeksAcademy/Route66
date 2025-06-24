5e6587615ca73fb4b87018490783fb9f24f6f960
Tonyreng <tonyreng01@gmail.com>
2025-06-24 04:01:37 +0000

    Se creo la pagina LoadsBoard para ir creando, agregando y probando los componentes necesarios

---
+++ src/front/pages/LoadsBoard.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react"
+import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
+export const LoadsBoard = () => {
+
+    const { store, dispatch } = useGlobalReducer()
+
+
+
+
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    );
+};
