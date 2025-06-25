2f7a4ab3445060bddce49fe727f273c48084f4e4
Daniela-MV <danmv.0991@gmail.com>
2025-06-25 01:34:24 +0000

    Endpoint y fetch

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,11 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User,Load
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,49 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/load_register', methods=['POST'])
+@jwt_required()
+def loads_register():
+    claims = get_jwt()
+    user_role = claims.get("role") 
+
+    if user_role != "broker":
+        return jsonify({"msg": "No tienes permiso para registrar cargas"}), 403
+    
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"msg": "No se recibieron datos necesarios"}), 400
+
+    required_fields=[
+        "vehicle_year", "vehicle_make", "vehicle_model", "pickup_Location", "payment","days_to_deliver"]
+    
+    if not all(field in data for field in required_fields):
+        return jsonify({"msg": "Faltan datos obligatorios"}), 400
+    
+    vehicle_year = data['vehicle_year']
+    vehicle_make = data['vehicle_make']
+    vehicle_model = data['vehicle_model']
+    pickup_Location = data['pickup_Location']
+    payment = data['payment']
+    days_to_deliver = data['days_to_deliver']
+
+    new_load = Load(
+        vehicle_year =vehicle_year,
+        vehicle_make=vehicle_make,
+        vehicle_model= vehicle_model,
+        pickup_Location=pickup_Location,
+        payment=payment,
+        days_to_deliver=days_to_deliver
+        )
+
+    db.session.add(new_load)
+    try:
+        db.session.commit()
+        return jsonify({"msg": "Carga registrada exitosamente."}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"msg": "Error al registrar la carga", "error": str(e)}), 500
+
---
+++ src/front/pages/LoadRegister.jsx
@@ -0,0 +1,161 @@
+// Import necessary components from react-router-dom and other parts of the application.
+//import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+export const LoadRegister = () => {
+
+	const initialForm = {
+		year: "",
+		make: "",
+		model: "",
+		pickupLocation: "", 
+		deliveryLocation: "",
+		payment: "",
+		daysToDeliver: "",
+	}
+
+	const [form, setForm] = useState(initialForm)
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+
+		setForm((prevForm) => ({
+			...prevForm,
+			[name]: value,
+		}));
+	}
+
+
+	async function registerLoad(form) {
+		const userData = {
+			vehicle_year:form.year,
+			vehicle_make:form.make,
+			vehicle_model:form.model,
+			pickup_Location:form.pickupLocation,
+			delivery_location:form.deliveryLocation,
+			payment:form.payment,
+			days_to_deliver:form.daysToDeliver,
+		};
+		console.log(userData);
+		try {
+			const response = await fetch(`${backendUrl}/api/load_register`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(userData),
+			});
+
+			const result = await response.json();
+
+			if (response.ok) {
+				console.log('Registro de carga exitoso');
+
+				setForm(initialForm);
+
+				return result;
+			} else {
+				console.error('Error en el registro de carga:', result.msg);
+				alert(`Error al registrar carga: ${result.msg}`);
+				return null;
+			}
+		} catch (error) {
+			console.error('Error de red o del servidor:', error);
+			return null;
+		}}
+
+
+		return (
+			<div>
+				<div className="container text-center border border-secondary-subtle border-4 pt-4 mt-4 bg-white rounded-3" style={{ width: "70%", height: "350px" }}>
+
+					<div className="mb-2 fw-bold" id="titulo">
+						LOAD REGISTER
+					</div>
+					<form className="row g-3">
+						<div className="col-md-4">
+							<label htmlFor="inputYear" className="form-label fw-bold text-primary-emphasis">Vehicle Year</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputYear"
+								name="year"
+								value={form.year}
+								onChange={handleChange} />
+						</div>
+						<div className="col-md-4">
+							<label htmlFor="inputMake" className="form-label fw-bold text-primary-emphasis">Vehicle Make</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputMake"
+								name="make"
+								value={form.make}
+								onChange={handleChange} />
+						</div>
+						<div className="col-md-4">
+							<label htmlFor="inputModel" className="form-label fw-bold text-primary-emphasis">Vehicle Model</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputModel"
+								name="model"
+								value={form.model}
+								onChange={handleChange} />
+						</div>
+						<div className="col-md-3">
+							<label htmlFor="inputPk" className="form-label fw-bold text-primary-emphasis">Pickup Location</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputPk"
+								name="pickupLocation"
+								value={form.pickupLocation}
+								onChange={handleChange} />
+						</div>
+						<div className="col-md-3">
+							<label htmlFor="inputDelivery" className="form-label fw-bold text-primary-emphasis">Delivery Location</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputDelivery"
+								name="deliveryLocation"
+								value={form.deliveryLocation}
+								onChange={handleChange} />
+						</div>
+						<div className="col-md-3">
+							<label htmlFor="inputPayment" className="form-label fw-bold text-primary-emphasis">Payment</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputPayment"
+								name="payment"
+								value={form.payment}
+								onChange={handleChange} />
+						</div>
+						<div className="col-md-3">
+							<label htmlFor="inputDtd" className="form-label fw-bold text-primary-emphasis">Days to delivery</label>
+							<input
+								type="text"
+								className="form-control border border-danger"
+								id="inputDtd"
+								name="daysToDeliver"
+								value={form.daysToDeliver}
+								onChange={handleChange} />
+						</div>
+
+
+						<div className="col-12">
+							<button
+								type="button"
+								className="boton fs-4 rounded-3"
+								style={{ width: "200px", height: "70px" }}
+							    onClick={() => registerLoad(form)}>Created Order</button>
+						</div>
+					</form >
+				</div >
+			</div>
+		);
+	};
