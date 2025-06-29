3d4d35cacc6126adafe1e5d61f17d07530afcfb4
Tonyreng <tonyreng01@gmail.com>
2025-06-29 00:25:22 +0000

    Actualizando a develop

--- src/front/components/Header.jsx
+++ src/front/components/Header.jsx
@@ -15,74 +15,69 @@ export const Header = ({
     const location = useLocation();
     const navigate = useNavigate();
 
-    const HomeButtons = () => {
-        return (
-            <nav className="d-flex align-items-center gap-3">
-                <Button variant="contained" endIcon={<LoginIcon />} color="error" sx={{ height: 'fit-content' }} onClick={() => {
-                    navigate("/login");
-                }}>
-                    Login
-                </Button>
-                <div className="dropdown">
-                    <button className="btn btn-danger fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Started
-                    </button>
-                    <ul className="dropdown-menu dropdown-menu-end">
-                        <li>
-                            <Link to="/register/broker">I'm a Broker</Link>
-                        </li>
-                        <li>
-                            <Link to="/register/carrier">I'm a Carrier</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+    const HomeButtons = () => (
+        <nav className="d-flex align-items-center gap-3">
+            <Button variant="contained" endIcon={<LoginIcon />} color="error" sx={{ height: 'fit-content' }} onClick={() => {
+                navigate("/login");
+            }}>
+                Login
+            </Button>
+            <div className="dropdown">
+                <button className="btn btn-danger fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Started
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <Link to="/register/broker">I'm a Broker</Link>
+                    </li>
+                    <li>
+                        <Link to="/register/carrier">I'm a Carrier</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 
-    const LoginButton = () => {
-        return (
-            <nav className="d-flex align-items-center gap-3">
-                <div className="dropdown">
-                    <button className="btn btn-danger fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Started
-                    </button>
-                    <ul className="dropdown-menu dropdown-menu-end">
-                        <li>
-                            <Link to="/register/broker">I'm a Broker</Link>
-                        </li>
-                        <li>
-                            <Link to="/register/carrier">I'm a Carrier</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
 
-    const RegisterButton = () => {
-        return (
-            <nav className="d-flex align-items-center gap-3">
-                <Button variant="contained" endIcon={<LoginIcon />} color="error" sx={{ height: 'fit-content' }} onClick={() => {
-                    navigate("/login");
-                }}>
-                    Login
-                </Button>
-            </nav>
-        )
-    }
+    const LoginButton = () => (
+        <nav className="d-flex align-items-center gap-3">
+            <div className="dropdown">
+                <button className="btn btn-danger fw-bold dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Started
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <Link to="/register/broker">I'm a Broker</Link>
+                    </li>
+                    <li>
+                        <Link to="/register/carrier">I'm a Carrier</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 
-    const SesionsButton = () => {
-        return (
-            <nav className="d-flex align-items-center gap-3">
-                <Button variant="contained" endIcon={<LogoutIcon />} color="error" sx={{ height: 'fit-content' }} onClick={() => {
-                    navigate("/");
-                }}>
-                    Logout
-                </Button>
-            </nav >
-        )
-    }
+
+    const RegisterButton = () => (
+        <nav className="d-flex align-items-center gap-3">
+            <Button variant="contained" endIcon={<LoginIcon />} color="error" sx={{ height: 'fit-content' }} onClick={() => {
+                navigate("/login");
+            }}>
+                Login
+            </Button>
+        </nav>
+    )
+
+    const SesionsButton = () => (
+        <nav className="d-flex align-items-center gap-3">
+            <Button variant="contained" endIcon={<LogoutIcon />} color="error" sx={{ height: 'fit-content' }} onClick={() => {
+                navigate("/");
+            }}>
+                Logout
+            </Button>
+        </nav >
+    )
+
 
     return (
         <Box sx={containerStyle}>
